992cc8aebb9189e79029c1a552f8ef630c71057d
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 04:07:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000 
+    @status = "open"
+  end
 
+  def deposit(cash)
+    @balance(cash)
+  end
 end
