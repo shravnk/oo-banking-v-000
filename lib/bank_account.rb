3279cb956bc6ab8c8332d24f06c36a970e08bea1
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 04:09:41 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,22 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
-  
+
   def initialize(name)
     @name = name
-    @balance = 1000 
+    @balance = 1000
     @status = "open"
   end
 
   def deposit(cash)
-    @balance(cash)
+    @balance += cash
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    true if @status == "open" && @balance > 0
   end
 end
