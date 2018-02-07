bcaa436795e765540fa7c4f6a59fd52ae6bcf96a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 04:11:13 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -23,5 +23,5 @@ class BankAccount
   def close_account
     @status = "closed"
   end
-  
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount
+  def initialize(sender, receiver, amount)
 end
