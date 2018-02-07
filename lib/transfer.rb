1f2ee05388d58fb57ea2015c196117793fae586a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 04:13:59 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,15 @@
 class Transfer
   # your code here
   attr_reader :sender, :receiver, :amount
+  attr_accessor :status
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @status = pending
   end
+
+def valid?
+  true if @sender.instance_of? BankAccount && @receiver.instance_of? BankAccount
+  
 end
